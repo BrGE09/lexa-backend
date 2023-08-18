908399c5b3a63a15384605b028beccdcc1e20874
lsllslsl <3519110077@uth.edu.mx>
2023-08-18 16:38:25 -0600

    document

--- app/controllers/documents.controller.php
+++ app/controllers/documents.controller.php
@@ -2,7 +2,7 @@
 
 require_once("../../database/database.php");
 
-class RecordsController extends BaseDatos
+class RecordsController extends BssaseDatos
 {
     public function createDocument($dname, $rout, $flSize, $reg_date, $rd_time, $idRd, $idFl){
 
@@ -20,13 +20,37 @@ class RecordsController extends BaseDatos
 
     public function viewDocument($id){
 
+        try {
+            $query = "SELECT * FROM  id_document WHERE id_document = $id";
+
+            $response = $this->consulta($query);
+
+            return $response->fetch_object();
+
+        } catch (Exception $e) {
+            echo ("Error: Ocurrió un error inesperado: " . $e);
+        }
     }
 
     public function updateDocument($id){
 
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
     }
 
     public function deleteDocument($id){
+
+        try {
+        $query = "DELETE FROM tbl_records WHERE id_document = $id";
+        $response = $this->consulta($query);
+        } catch (Excception $e) {
+            echo ("Error: Ocurrió un error inesperado: " . $e);
+
+        }
 
     }
 }
---
+++ app/models/document.model.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once("../controllers/documents.controller.php");
+
+    class new Document(){
+
+        $dname = $_REQUEST[''],
+        $rout = $_REQUEST[''],
+        $flSize = $_REQUEST[''],
+        $reg_date = $_REQUEST[''],
+        $rd_time = $_REQUEST[''],
+        $idRd = $_REQUEST[''],
+        $idFl = $_REQUEST['']
+
+        
+    }
+
+
+?>
