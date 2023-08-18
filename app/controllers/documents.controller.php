37ea1c3ab749c4b79f32ab5c51217345d0dad01f
lsllslsl <3519110077@uth.edu.mx>
2023-08-18 14:57:11 -0600

    document

---
+++ app/controllers/documents.controller.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("../../database/database.php");
+
+class RecordsController extends BaseDatos
+{
+    public function createDocument(){
+
+
+        try {
+            $query = "INSERT INTO  tbl_document (id_document, documente_name, route, file_size, registration_date, record_time, id_record, id_file) VALUES ('$dname','$rout', '$flSize','$reg_date','$rd_time','$idRd','$idFl',)";
+
+            if ($this->consulta($query)) {
+                header("Location: ...")
+            }
+        } catch (Exception $e ) {
+            echo ("Error: Ocurrió un error inesperado:" . $e);
+        }
+    }
+
+    public function viewDocument($id){
+
+    }
+
+    public function updateDocument($id){
+
+    }
+
+    public function deleteDocument($id){
+
+    }
+}
+
+
+?>
