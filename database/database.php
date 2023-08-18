6150a070b9e1ea22b76e6cee32cddd7fae639b98
BrandonGE09 <brandon__guerra@hotmail.com>
2023-08-18 13:31:51 -0600

    Conexión

---
+++ database/database.php
@@ -0,0 +1,33 @@
+<?php
+
+class BaseDatos{
+
+    private $mysqli;
+
+    public function __construct(){
+
+        $servidor = "localhost";
+        $usuario = "root";
+        $pass = "";
+        $database = "lexa";
+
+        $this->mysqli = new mysqli($servidor, $usuario, $pass, $database);
+
+        if(mysqli_connect_errno()){
+            printf("La conexión falló");
+        }
+    }
+
+    public function consulta($query){
+        return $this->mysqli->query($query);
+    }
+
+    public function __destruct()
+    {
+        $this->mysqli->close();
+    }
+
+
+
+}
+?>
